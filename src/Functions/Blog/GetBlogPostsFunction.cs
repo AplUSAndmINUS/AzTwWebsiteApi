b66a711b2178c5163642100472a6870c13e98615
Terence Waters <12157591+AplUSAndmINUS@users.noreply.github.com>
2025-05-25 10:34:24 -0600

    implementing the table storage for blog posts access

--- src/Functions/Blog/GetBlogPostsFunction.cs
+++ src/Functions/Blog/GetBlogPostsFunction.cs
@@ -5,6 +5,8 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Azure;
+using Azure.Data.Tables;
 using AzTwWebsiteApi.Utils;
 using AzTwWebsiteApi.Models.Blog;
 
